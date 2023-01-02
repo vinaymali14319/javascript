9932024fe5b05ef5158f84a66c5b384518326c8f
vinay mali <vinaymali14319@gmail.com>
2023-01-02 22:43:24 +0530

    1st q

---
+++ D21.js
@@ -0,0 +1,16 @@
+
+
+const prompt = require("prompt-sync")()
+
+let arr = [1,5,9,76,3,1,5,616]
+let number = prompt("Enter number")
+
+console.log(number)
+c= Number.parseInt(number)
+
+arr.push(c)
+
+console.log(arr)
+
+
+
