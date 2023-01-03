7c15956d4825b84fb4e9502dd70f0cc8c4454179
vinay mali <vinaymali14319@gmail.com>
2023-01-03 22:35:53 +0530

    D29

---
+++ D28/D28.js
@@ -0,0 +1,4 @@
+window.console.log(window)
+console.log(document.body)
+
+document.body.style.background= "pink"
---
+++ D29/D29.js
@@ -0,0 +1,61 @@
+// let age = prompt("enter age")
+
+// age = Number.parseInt(age)
+
+// if (age>18){
+
+//     alert("yes, you can drive")
+// }
+
+// else {
+//     alert("You can't drive")
+// }
+
+// ============================================================================
+// let runAgain = true;
+
+// const canDrive = (age) => {
+//   return age >= 18 ? true : false
+// }
+
+// while (runAgain) {
+//   let age = prompt("Enter your age")
+//   age = Number.parseInt(age)
+//   if (age < 0) {
+//     console.error("Please enter a valid age");
+//     break;
+//   }
+
+//   if (canDrive(age)) {
+//     alert("Yes you can drive")
+//   }
+//   else {
+//     alert("You cannot drive")
+//   }
+//   runAgain = confirm("Do you want to play again?")
+// }
+
+
+// =========================================================================
+
+
+// let number = prompt("Enter your number")
+
+// number= Number.parseInt(number)
+
+// if (number>4){
+//     location.href = "https://google.com"
+// }
+// else {
+//     location.href = "https://www.youtube.com/watch?v=gvVmcKiErx4"
+// }
+
+// ==============================================================================
+
+
+
+
+
+
+let color = prompt("Enter the page background color")
+document.body.style.background = color
