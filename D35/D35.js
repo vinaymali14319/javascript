f39274ef8da2242e266947cbb8ba75a515946ce4
vinay mali <vinaymali14319@gmail.com>
2023-01-09 22:24:42 +0530

    D36

---
+++ D35/D35.js
@@ -0,0 +1,5 @@
+let t  = document.body.firstElementChild.firstElementChild
+
+console.log(t)
+
+console.log(t.rows)
--- D36/D34.js
+++
@@ -1,5 +0,0 @@
-let b =document.body
-
-console.log ("First child of b is ",b.firstChild)
-
-console.log ("First Element child of b is ",b.firstElementChild)
