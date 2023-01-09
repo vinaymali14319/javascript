70e4796fd4a64623ce26830defe57082b95628ac
vinay mali <vinaymali14319@gmail.com>
2023-01-09 23:01:05 +0530

    Q4

--- D38/D38.js
+++ D38/D38.js
@@ -1,3 +1,7 @@
 document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red"
 document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green"
 document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green"
+
+Array.from(document.getElementsByTagName("li")).forEach((element) =>{
+    element.style.background = "pink";
+  })     
